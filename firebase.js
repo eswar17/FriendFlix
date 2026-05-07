282577a53defa7aac8591bcdbb95cf8751a654af
eswar17 <46845800+eswar17@users.noreply.github.com>
2026-05-07 19:24:43 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,19 +1,44 @@
 // firebase.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+
+// ==========================================
+// FIREBASE CONFIG
+// ==========================================
+
 const firebaseConfig = {
+
   apiKey: "AIzaSyCEtiDilDHSXpucdC_oPW-vgBYnsgIv1Qw",
+
   authDomain: "friendflix-8c346.firebaseapp.com",
+
   projectId: "friendflix-8c346",
+
   storageBucket: "friendflix-8c346.firebasestorage.app",
+
   messagingSenderId: "716663178628",
-  appId: "1:716663178628:web:1fc686217973726aafc9d1",
-  measurementId: "G-D29S5N66QL"
+
+  appId: "1:716663178628:web:1fc686217973726aafc9d1"
+
 };
+
+
+// ==========================================
+// INITIALIZE FIREBASE
+// ==========================================
 
 const app = initializeApp(firebaseConfig);
 
+
+// ==========================================
+// EXPORT SERVICES
+// ==========================================
+
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
